e31ca7cdb36e989826daf9d08e843e43034a8f9b
Edison-Enrique Crespo-Vasquez <edison.crespo.ext@inetum.com>
2025-02-27 20:41:42 +0100

    add axios for Appointment/Check

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,14 @@
 <script>
+import axios from "axios";
+
 export default {
   name: "HomeUser",
+  data() {
+    return {
+      bookings: [],
+      message: "",
+    };
+  },
   computed: {
     Username() {
       return this.$store.state.username;
@@ -14,16 +22,50 @@ export default {
       this.$store.commit("setAuthentication", false);
       this.$router.push("/");
     },
+    CheckBookings() {
+      var email = localStorage.getItem("email");
+      axios
+        .get("https://localhost:7093/api/Appointment/CheckCheck/", {
+          params: { email: email },
+        })
+        .then((response) => {
+          this.message = response.data.message[0];
+          this.bookings = response.data.bookings;
+        })
+        .catch((error) => {
+          alert("Algo anda mal...");
+          console.log(error);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <div class="content">
+    <nav class="navegation">
+      <button @click="CheckBookings">Check Booking</button>
+      <button @click="logout">Cerrar sesión</button>
+    </nav>
+    <br />
     <h1>Bienvenido {{ Username }}</h1>
     <br />
     <p>Datos personales: {{ Email }}</p>
-    <button @click="logout">Cerrar sesión</button>
+
+    <p>{{ message }}</p>
+    <table border="1">
+      <tr>
+        <td>Name</td>
+        <td>Service</td>
+        <td>Date</td>
+      </tr>
+
+      <tr v-for="(booking, index) in bookings" :key="index">
+        <td>{{ booking.name }}</td>
+        <td>{{ booking.service }}</td>
+        <td>{{ booking.date }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -33,5 +75,26 @@ export default {
   place-items: center;
   margin: auto;
   font-family: monospace;
+}
+
+.navegation {
+  background-color: rgb(72, 72, 172);
+  padding: 1%;
+  width: 90%;
+}
+
+.navegation button {
+  border: none;
+  background: none;
+  color: white;
+  font-size: 15px;
+  margin-left: 10px;
+  transition: all 0.3s ease-in-out;
+}
+
+.navegation button:hover {
+  transform: scale(1.1);
+  border-bottom: 1px solid rgb(255, 255, 255);
+  background-color: rgb(0, 0, 0, 0.5);
 }
 </style>
